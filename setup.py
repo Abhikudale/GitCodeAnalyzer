4aa32eae04fd4f6ece76039133e5b8ae00d826ce
Abhi <cv004700@gmail.com>
2024-07-15 17:25:54 +0530

    adding new file and folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'AbhinandanKudale',
+    author_email= 'cv004700@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
